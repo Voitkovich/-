5e91e31cef36acd023521423d2df67f4aec86ceb
Ekaterina Voitkovich <styardess@mail.ru>
2022-12-02 11:26:28 +0300

    final

--- string Array/Program.cs
+++ string Array/Program.cs
@@ -1,3 +1,27 @@
-﻿string[] array1 = new string[5] {"hello", "wow", "yes", "747", "777"};
+﻿
+
+string[] array1 = new string[5] {"hello", "wow", "yes", "B747", "B777"};
 string[] array2 = new string[array1.Length];
 
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void Printarray(string[] array)
+{
+    {
+        Console.WriteLine($"[{string.Join(", ", array1)}] -> [{string.Join(", ", array2)}]");
+    }
+    Console.WriteLine();
+}
+
+NewArray(array1, array2);
+Printarray(array2);
